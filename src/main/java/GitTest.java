6d2305d1f2bfc579e15b0f06a6a59cf2d8267800
saxon <saxon957@163.com>
2023-08-13 19:05:27 +0800

    my second commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -10,5 +10,8 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
     }
 }
