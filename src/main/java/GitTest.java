510c02f4aa2a0f5a63d1de567c4c29a622ed21e2
saxon <saxon957@163.com>
2023-08-13 18:57:09 +0800

    my first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,14 @@
+/**
+ * ClassName:GitTest
+ * Package:PACKAGE_NAME
+ * Description:
+ *
+ * @Author Saxon
+ * @Create2023/8/1318:50
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
